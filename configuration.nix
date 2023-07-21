ddcb5750d48bc9a3c121de375e4ab4bd4005b2ba
AVAADMIN <admin@localpc.com>
2023-07-21 11:49:47 -0300

    Initial local version

---
+++ configuration.nix
@@ -0,0 +1,189 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, ... }:
+
+let
+   unstable = import <unstable> {};
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  #boot.loader.grub.efiInstallAsRemovable = true;
+  #boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.grub.useOSProber = true;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Enable the X11 windowing system.
+   services.xserver.enable = true;
+   services.xserver.displayManager.startx.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.layout = "br";
+  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  # Enable pipewire instead of pulseaudio
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  security.rtkit.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.groups.i2c.gid = 1000;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.alexvanaxe = {
+     uid=1000; # The uid has to be the same as arch
+     isNormalUser = true;
+     extraGroups = [ "wheel" "video" "i2c" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+
+     ];
+   };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.alexfunaxe = {
+     uid=1001;
+     isNormalUser = true;
+     extraGroups = [ "wheel" "video" "i2c" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+
+     ];
+   };
+
+  programs.light.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     # Desktops apps
+     light
+     # Control the monitor
+     ddcutil
+
+     libnotify
+     #utilities
+     bc
+     vim
+     wget
+     alacritty
+     git
+     firefox
+     pavucontrol
+
+     # Helpers
+    tldr
+    
+
+     # Adminitration
+     killall
+     htop
+     xorg.xbacklight
+
+     # Installing the new version of the e2fsck
+     unstable.e2fsprogs
+
+     # monitors
+     lm_sensors
+   ];
+
+  # To enable the font we need to put it in a different config.
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    symbola
+    font-awesome
+    hack-font
+    roboto-mono
+    hermit
+    mononoki
+    overpass
+    terminus_font
+    orbitron
+
+    jetbrains-mono
+    (nerdfonts.override { fonts = [ "Iosevka" "NerdFontsSymbolsOnly" "CascadiaCode" ]; })
+
+    (import ./fonts.nix)
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+   system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
---
+++ fonts.nix
@@ -0,0 +1,44 @@
+with import <nixpkgs> {}; # bring all of Nixpkgs into scope
+
+stdenvNoCC.mkDerivation rec {
+  pname = "ava-fonts";
+  version = "1.003";
+
+  src = fetchzip {
+    url = "https://github.com/alexvanaxe107/fonts/archive/master.zip";
+    hash = "sha256-A3QlSHNAMOf1wW8PUb9XqnIeYslkpa0mu9NNgJi5Iv8=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 *.ttf -t $out/share/fonts/wmfontsttf
+    install -Dm644 *.otf -t $out/share/fonts/wmfontsotf
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "A typeface designed for source code";
+    longDescription = ''
+      Hack is hand groomed and optically balanced to be a workhorse face for
+      code. It has deep roots in the libre, open source typeface community and
+      expands upon the contributions of the Bitstream Vera & DejaVu projects.
+      The face has been re-designed with a larger glyph set, modifications of
+      the original glyph shapes, and meticulous attention to metrics.
+    '';
+    homepage = "https://sourcefoundry.org/hack/";
+
+    /*
+      "The font binaries are released under a license that permits unlimited
+      print, desktop, and web use for commercial and non-commercial
+      applications. It may be embedded and distributed in documents and
+      applications. The source is released in the widely supported UFO format
+      and may be modified to derive new typeface branches. The full text of
+      the license is available in LICENSE.md" (From the GitHub page)
+    */
+    license = licenses.free;
+    maintainers = with maintainers; [ "alexvanaxe" ];
+    platforms = platforms.all;
+  };
+}
