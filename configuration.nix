c0a7c7a65a5463c246836eacd63ca8504a1d1ebd
avadimin@ <avadimin@teste.com>
2023-09-05 14:01:39 -0300

    Changing DNS server

--- configuration.nix
+++ configuration.nix
@@ -184,7 +184,7 @@
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 8096 ];
-  networking.nameservers = [ "208.67.222.222" "208.67.220.220" ]
+  networking.nameservers = [ "208.67.222.222" "208.67.220.220" ];
 
 
   # networking.firewall.allowedUDPPorts = [ ... ];
