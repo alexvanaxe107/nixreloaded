e2eb4c031dd14136d2e4d2e3d678d5145a1fc3c7
Ava Note <ava@note.com>
2023-09-16 21:41:58 -0300

    Activating bluetooth

--- note/local-configs.nix
+++ note/local-configs.nix
@@ -6,6 +6,8 @@
 {
   networking.hostName = "palauai"; # Define your hostname.
 
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
