7db46fdc04f6c17354d35e5cd364a0bfe698fb79
avadimin@ <avadimin@teste.com>
2023-12-01 13:43:31 -0300

    Upgrading nix to 23.11

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
     # Official NixOS package source, using nixos-unstable branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixunstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # home-manager, used for managing user configuration
 #    home-manager = {
